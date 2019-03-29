c306d88bd5c68854f2124d1fee9099c7394ae01d
Stuart Mees <stuartmees@gmail.com>
2019-03-29 14:41:02 +0000

    added Pacdots

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
   const walls = [0,1,2,3,4,5,6,7,8,9,10,16,19,20,21,22,23,26,29,36,40,46,49,50,59,60,69,70,73,74,75,76,77,79,80,89,90,91,92,93,94,95,96,97,98,99]
 
-  const pacDots = [11,12,13,14,24,34,44,54,55,56,57,58,48,38,39,30]
+  const pacDots = [12,13,14,24,34,44,54,55,56,57,58,48,38,39,30]
 
 
   //Creates the grid boxes and adds them to the grid
@@ -30,10 +30,10 @@ window.addEventListener('DOMContentLoaded', function(){
   })
 
   pacDots.forEach(function(dotIndex){
-    gridBoxes[dotIndex].classList.add('dot')
+    const pacDot = document.createElement('span')
+    gridBoxes[dotIndex].appendChild(pacDot)
+    pacDot.classList.add('pac-dot')
   })
-
-
 
   function move(){
     const player = gridBoxes.find(box => box.classList.contains('pacman'))
