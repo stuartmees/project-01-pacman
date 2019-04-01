97f31df3c17e79fce29584a507d1d3f1fd553fe6
Stuart Mees <stuartmees@gmail.com>
2019-04-01 11:45:43 +0100

    blue ghosts fully interact with pacman and reset correctly

--- js/app.js
+++ js/app.js
@@ -4,23 +4,32 @@ window.addEventListener('DOMContentLoaded', function(){
 
   const width = 10 //Set the width of the game grid
   let position = 11 //Set the starting position and define the position variable to store the current position of pacman
-  let redGhostPosition = 65
+  let redGhostPosition = 44
+  let yellowGhostPosition = 45
+  let blueGhostYellowPosition
+  let blueGhostRedPosition
   let score = 0 //Initialise player score variable
   let lives = 3
+  let powerScore = 200
   const gridBoxes = [] //Initialise the array to store the gridBoxes in
   let keyCode //Initialise the keyCode variable to store the code of the key when pressed
   // let interval //Initialise the  variable to store the setInterval method
-  let newPosition //Initialise the newPOsition variable. This allows new position to be checked for validity beofre setting as position
+  // let newPosition //Initialise the newPOsition variable. This allows new position to be checked for validity beofre setting as position
   const numberOfBoxes = width*width
 
   let redGhostInterval
+  let yellowGhostInterval
+  let blueGhostYellowInterval
+  let blueGhostRedInterval
 
   const scoreElem = document.querySelector('#score')// Get the score element
   const livesElem  = document.querySelector('#lives')// Get the lives element
 
-  const walls = [0,1,2,3,4,5,6,7,8,9,10,16,19,20,21,22,23,26,29,36,40,46,49,50,59,60,69,70,73,74,75,76,77,79,80,89,90,91,92,93,94,95,96,97,98,99] //Store which grid boxes are wall
+  const walls = [0,1,2,3,4,5,6,7,8,9,10,19,20,29,40,43,46,49,50,53,54,55,56,59,60,69,70,79,80,89,90,91,92,93,94,95,96,97,98,99] //Store which grid boxes are wall
 
-  const pacDots = [12,13,14,24,34,44,54,55,56,57,58,48,38,39,30] //Store where the Pacdot are positioned
+  const pacDots = [12,13,14,24] //Store where the Pacdot are positioned
+
+  const powerDots = [88]
 
 
   //Creates the grid boxes and adds them to the game grid
@@ -35,6 +44,7 @@ window.addEventListener('DOMContentLoaded', function(){
   gridBoxes[position].classList.add('pacman')//Puts Pacman wherever the the position dictates
 
   gridBoxes[redGhostPosition].classList.add('red-ghost')//Puts redGhost wherever  redGhost position dictates
+  gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
 
   //Sets the relvant grid boxes as part of the wall
   walls.forEach((wallIndex) => gridBoxes[wallIndex].classList.add('wall'))
@@ -42,33 +52,93 @@ window.addEventListener('DOMContentLoaded', function(){
   livesElem.innerText = lives
 
   //Makes a pac dot element
-  function makePacDot(dotIndex){
-    const pacDot = document.createElement('span')
-    gridBoxes[dotIndex].appendChild(pacDot)
-    pacDot.classList.add('pac-dot')
+  pacDots.forEach(function(dotIndex){
+    gridBoxes[dotIndex].classList.add('pac-dot')
+  })
+
+  powerDots.forEach(function(dotIndex){
+    gridBoxes[dotIndex].classList.add('power-dot')
+  })
+
+  // function eatDot(pacmanGridBox){
+  //   pacmanGridBox.classList.remove('pac-dot')
+  //   score++
+  //   scoreElem.innerText = score
+  // }
+
+  function endBlueGhost(){
+    gridBoxes[blueGhostRedPosition].classList.remove('blue-ghost-red')
+    gridBoxes[blueGhostYellowPosition].classList.remove('blue-ghost-yellow')
+    clearInterval(blueGhostYellowInterval)
+    clearInterval(blueGhostRedInterval)
+    yellowGhostPosition = blueGhostYellowPosition
+    redGhostPosition = blueGhostRedPosition
+    blueGhostRedPosition = null
+    blueGhostYellowPosition = null
+    gridBoxes[redGhostPosition].classList.add('red-ghost')
+    gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+    powerScore = 200
+    redGhostMove()
+    redGhostInterval = setInterval(redGhostMove, 500)
+    yellowGhostMove()
+    yellowGhostInterval = setInterval(yellowGhostMove, 500)
   }
 
-  function eatDot(node){
-    if (node.classList.contains('pac-dot')){
-      node.parentNode.removeChild(node)
-      score++
-      scoreElem.innerText = score
-    }
+  function startBlueGhost(pacmanGridBox){
+    pacmanGridBox.classList.remove('power-dot')
+    clearInterval(redGhostInterval)
+    clearInterval(yellowGhostInterval)
+    blueGhostYellowPosition = yellowGhostPosition
+    blueGhostRedPosition = redGhostPosition
+    gridBoxes[redGhostPosition].classList.remove('red-ghost')
+    gridBoxes[blueGhostRedPosition].classList.add('blue-ghost-red')
+    gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+    gridBoxes[blueGhostYellowPosition].classList.add('blue-ghost-yellow')
+    yellowGhostPosition = null
+    redGhostPosition = null
+    blueGhostYellowMove()
+    blueGhostYellowInterval = setInterval(blueGhostYellowMove, 1000)
+    blueGhostRedMove()
+    blueGhostRedInterval = setInterval(blueGhostRedMove, 1000)
+    setTimeout(endBlueGhost, 10000)
   }
-
   //Universal function that moves Pacman in the correct direction when invoked
   function move(){
     const pacmanGridBox = gridBoxes[position]
     const player = gridBoxes.find(box => box.classList.contains('pacman'))
     player.classList.remove('pacman')
     pacmanGridBox.classList.add('pacman')
-    pacmanGridBox.childNodes.forEach(eatDot)
-    redGhostReset()
+    if (pacmanGridBox.classList.contains('pac-dot')){
+      pacmanGridBox.classList.remove('pac-dot')
+      score++
+      scoreElem.innerText = score
+    }
+    if (pacmanGridBox.classList.contains('power-dot')) startBlueGhost(pacmanGridBox)
+    if ((pacmanGridBox.classList.contains('blue-ghost-yellow')) || (pacmanGridBox.classList.contains('blue-ghost-red'))){
+      score = score + powerScore
+      powerScore = powerScore*2
+      scoreElem.innerText = score
+      if (pacmanGridBox.classList.contains('blue-ghost-yellow')){
+        clearInterval(blueGhostYellowInterval)
+        const blueGhostYellow = gridBoxes.find(box => box.classList.contains('blue-ghost-yellow'))
+        blueGhostYellow.classList.remove('blue-ghost-yellow')
+        blueGhostYellowPosition = 45
+      }
+      if (pacmanGridBox.classList.contains('blue-ghost-red')){
+        clearInterval(blueGhostRedInterval)
+        const blueGhostRed = gridBoxes.find(box => box.classList.contains('blue-ghost-red'))
+        blueGhostRed.classList.remove('blue-ghost-red')
+        blueGhostRedPosition = 44
+      }
+    }
+    if ((!!redGhostPosition && !!yellowGhostPosition) && ((gridBoxes[redGhostPosition].classList.contains('pacman') || gridBoxes[yellowGhostPosition].classList.contains('pacman')))) {
+      ghostReset()
+    }
   }
 
 
   function moveRight(){
-    newPosition = position+1
+    let newPosition = position+1
     newPosition = (newPosition % width === 0) ? (newPosition - width):newPosition
     if(gridBoxes[newPosition].classList.contains('wall')){
       // clearInterval(interval)
@@ -81,7 +151,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 
   function moveLeft(){
-    newPosition = position-1
+    let newPosition = position-1
     newPosition = ((newPosition+1) % width === 0) ? (newPosition + width):(newPosition)
     if(gridBoxes[newPosition].classList.contains('wall')){
       // clearInterval(interval)
@@ -93,7 +163,7 @@ window.addEventListener('DOMContentLoaded', function(){
   }
 
   function moveUp(){
-    newPosition = position - width
+    let newPosition = position - width
     if (newPosition < width){
       newPosition = newPosition + (numberOfBoxes-width)
     }
@@ -107,7 +177,7 @@ window.addEventListener('DOMContentLoaded', function(){
   }
 
   function moveDown(){
-    newPosition = position + width
+    let newPosition = position + width
     if (newPosition>=(numberOfBoxes-width) && newPosition<numberOfBoxes){
       newPosition = newPosition-(numberOfBoxes-width)
     }
@@ -120,77 +190,84 @@ window.addEventListener('DOMContentLoaded', function(){
     }
   }
 
-  function redGhostReset(){
-    if (gridBoxes[redGhostPosition].classList.contains('pacman')){
-      clearInterval(redGhostInterval)
-      gridBoxes[redGhostPosition].classList.remove('pacman')
-      gridBoxes[redGhostPosition].classList.remove('red-ghost')
-      lives--
-      score = 0
-      if (lives>0){
-        setTimeout(function(){
-          livesElem.innerText = lives
-          scoreElem.innerText = score
-          position = 11
-          redGhostPosition = 65
-          gridBoxes[position].classList.add('pacman')
-          gridBoxes[redGhostPosition].classList.add('red-ghost')
-          pacDots.forEach(function(dotIndex){
-            gridBoxes[dotIndex].childNodes.forEach(function(node){
-              if (node.classList.contains('pac-dot')){
-                node.parentNode.removeChild(node)
-              }
-            })
-          })
-          pacDots.forEach(function(dotIndex){
-            makePacDot(dotIndex)
-          })
-          redGhostInterval = setInterval(redGhostMove, 1000)
-        }, 2000)
-      } else {
-        livesElem.innerText = 'Game Over!'
-      }
+  function ghostSetUp(){
+    livesElem.innerText = lives
+    scoreElem.innerText = score
+    position = 11
+    redGhostPosition = 65
+    yellowGhostPosition = 66
+    gridBoxes[position].classList.add('pacman')
+    gridBoxes[redGhostPosition].classList.add('red-ghost')
+    gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+    pacDots.forEach(function(pacDotIndex){
+      if (!gridBoxes[pacDotIndex].classList.contains('pac-dot')) gridBoxes[pacDotIndex].classList.add('pac-dot')
+    })
+    redGhostInterval = setInterval(redGhostMove, 1000)
+    yellowGhostInterval = setInterval(yellowGhostMove, 1000)
+  }
 
+  function ghostReset(){
+    clearInterval(redGhostInterval)
+    clearInterval(yellowGhostInterval)
+    gridBoxes[position].classList.remove('pacman')
+    gridBoxes[redGhostPosition].classList.remove('red-ghost')
+    gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+    lives--
+    score = 0
+    if (lives>0){
+      setTimeout(ghostSetUp, 2000)
+    } else {
+      livesElem.innerText = 'Game Over!'
     }
   }
 
+
+
   function redGhostMoveRight(){
     const newPosition = redGhostPosition+1
-    if (!gridBoxes[newPosition].classList.contains('wall')){
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('yellow-ghost'))){
       gridBoxes[redGhostPosition].classList.remove('red-ghost')
       redGhostPosition++
       gridBoxes[redGhostPosition].classList.add('red-ghost')
-      redGhostReset()
+      if (gridBoxes[redGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
     }
   }
 
   function redGhostMoveLeft(){
     const newPosition = redGhostPosition -1
-    if (!gridBoxes[newPosition].classList.contains('wall')){
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('yellow-ghost'))){
       gridBoxes[redGhostPosition].classList.remove('red-ghost')
       redGhostPosition--
       gridBoxes[redGhostPosition].classList.add('red-ghost')
-      redGhostReset()
+      if (gridBoxes[redGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
     }
   }
 
   function redGhostMoveUp(){
     const newPosition  = redGhostPosition-width
-    if (!gridBoxes[newPosition].classList.contains('wall')){
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('yellow-ghost'))){
       gridBoxes[redGhostPosition].classList.remove('red-ghost')
       redGhostPosition = redGhostPosition - width
       gridBoxes[redGhostPosition].classList.add('red-ghost')
-      redGhostReset()
+      if (gridBoxes[redGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
     }
   }
 
   function redGhostMoveDown(){
     const newPosition = redGhostPosition+width
-    if (!gridBoxes[newPosition].classList.contains('wall')){
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('yellow-ghost'))){
       gridBoxes[redGhostPosition].classList.remove('red-ghost')
       redGhostPosition = redGhostPosition + width
       gridBoxes[redGhostPosition].classList.add('red-ghost')
-      redGhostReset()
+      if (gridBoxes[redGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
     }
   }
 
@@ -208,18 +285,174 @@ window.addEventListener('DOMContentLoaded', function(){
     }
   }
 
+  function yellowGhostMoveRight(){
+    const newPosition = yellowGhostPosition+1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('red-ghost'))){
+      gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+      yellowGhostPosition++
+      gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+      if (gridBoxes[yellowGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
+    }
+  }
 
+  function yellowGhostMoveLeft(){
+    const newPosition = yellowGhostPosition-1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('red-ghost'))){
+      gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+      yellowGhostPosition--
+      gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+      if (gridBoxes[yellowGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
+    }
+  }
 
+  function yellowGhostMoveUp(){
+    const newPosition  = yellowGhostPosition-width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('red-ghost'))){
+      gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+      yellowGhostPosition = yellowGhostPosition - width
+      gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+      if (gridBoxes[yellowGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
+    }
+  }
 
-  redGhostInterval = setInterval(redGhostMove, 1000)
-  
+  function yellowGhostMoveDown(){
+    const newPosition = yellowGhostPosition+width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('red-ghost'))){
+      gridBoxes[yellowGhostPosition].classList.remove('yellow-ghost')
+      yellowGhostPosition = yellowGhostPosition + width
+      gridBoxes[yellowGhostPosition].classList.add('yellow-ghost')
+      if (gridBoxes[yellowGhostPosition].classList.contains('pacman')){
+        ghostReset()
+      }
+    }
+  }
 
-  pacDots.forEach(makePacDot)//Puts a Pacdot in the relevant grid boxes
+  function yellowGhostMove(){
+    const direction = Math.floor(Math.random()*4)
+    switch(direction){
+      case 0: yellowGhostMoveRight()
+        break
+      case 1: yellowGhostMoveLeft()
+        break
+      case 2: yellowGhostMoveUp()
+        break
+      case 3: yellowGhostMoveDown()
+        break
+    }
+  }
+
+  function blueGhostYellowMoveRight(){
+    const newPosition = blueGhostYellowPosition+1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-yellow'))){
+      gridBoxes[blueGhostYellowPosition].classList.remove('blue-ghost-yellow')
+      blueGhostYellowPosition++
+      gridBoxes[blueGhostYellowPosition].classList.add('blue-ghost-yellow')
+    }
+  }
+
+  function blueGhostYellowMoveLeft(){
+    const newPosition = blueGhostYellowPosition-1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-yellow'))){
+      gridBoxes[blueGhostYellowPosition].classList.remove('blue-ghost-yellow')
+      blueGhostYellowPosition--
+      gridBoxes[blueGhostYellowPosition].classList.add('blue-ghost-yellow')
+    }
+  }
+
+  function blueGhostYellowMoveUp(){
+    const newPosition  = blueGhostYellowPosition-width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-yellow'))){
+      gridBoxes[blueGhostYellowPosition].classList.remove('blue-ghost-yellow')
+      blueGhostYellowPosition = blueGhostYellowPosition - width
+      gridBoxes[blueGhostYellowPosition].classList.add('blue-ghost-yellow')
+    }
+  }
+
+  function blueGhostYellowMoveDown(){
+    const newPosition = blueGhostYellowPosition+width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-yellow'))){
+      gridBoxes[blueGhostYellowPosition].classList.remove('blue-ghost-yellow')
+      blueGhostYellowPosition = blueGhostYellowPosition + width
+      gridBoxes[blueGhostYellowPosition].classList.add('blue-ghost-yellow')
+    }
+  }
+
+  function blueGhostYellowMove(){
+    const direction = Math.floor(Math.random()*4)
+    switch(direction){
+      case 0: blueGhostYellowMoveRight()
+        break
+      case 1: blueGhostYellowMoveLeft()
+        break
+      case 2: blueGhostYellowMoveUp()
+        break
+      case 3: blueGhostYellowMoveDown()
+        break
+    }
+  }
+
+  function blueGhostRedMoveRight(){
+    const newPosition = blueGhostRedPosition+1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-red'))){
+      gridBoxes[blueGhostRedPosition].classList.remove('blue-ghost-red')
+      blueGhostRedPosition++
+      gridBoxes[blueGhostRedPosition].classList.add('blue-ghost-red')
+    }
+  }
+
+  function blueGhostRedMoveLeft(){
+    const newPosition = blueGhostRedPosition -1
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-red'))){
+      gridBoxes[blueGhostRedPosition].classList.remove('blue-ghost-red')
+      blueGhostRedPosition--
+      gridBoxes[blueGhostRedPosition].classList.add('blue-ghost-red')
+    }
+  }
+
+  function blueGhostRedMoveUp(){
+    const newPosition  = blueGhostRedPosition-width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-red'))){
+      gridBoxes[blueGhostRedPosition].classList.remove('blue-ghost-red')
+      blueGhostRedPosition = blueGhostRedPosition - width
+      gridBoxes[blueGhostRedPosition].classList.add('blue-ghost-red')
+    }
+  }
+
+  function blueGhostRedMoveDown(){
+    const newPosition = blueGhostRedPosition+width
+    if ((!gridBoxes[newPosition].classList.contains('wall')) && (!gridBoxes[newPosition].classList.contains('blue-ghost-red'))){
+      gridBoxes[blueGhostRedPosition].classList.remove('blue-ghost-red')
+      blueGhostRedPosition = blueGhostRedPosition + width
+      gridBoxes[blueGhostRedPosition].classList.add('blue-ghost-red')
+    }
+  }
+
+  function blueGhostRedMove(){
+    const direction = Math.floor(Math.random()*4)
+    switch(direction){
+      case 0: blueGhostRedMoveRight()
+        break
+      case 1: blueGhostRedMoveLeft()
+        break
+      case 2: blueGhostRedMoveUp()
+        break
+      case 3: blueGhostRedMoveDown()
+        break
+    }
+  }
+
+  redGhostInterval = setInterval(redGhostMove, 500)
+  yellowGhostInterval = setInterval(yellowGhostMove, 500)
 
   //Invoke the relevant move funtion depending on which arrow key is pressed.
   document.addEventListener('keydown', function(e){
     keyCode = e.keyCode
-
     switch(keyCode){
       case 39:
         // clearInterval(interval)
